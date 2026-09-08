19cb6916c215de0f96bddf17fa587e5d4c85dae7
jaonayzzz <pakornpat9595@gmail.com>
2026-09-08 22:35:56 +0700

    พี่จ่าน้ำจะ

--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -1,29 +1,83 @@
 import TaskCard from "../components/TaskCard";
 import TodoModal from "../components/Modal";
 import { type TaskCardProps } from "../libs/Todolist";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+
+const defaultTasks: TaskCardProps[] = [
+  {
+    id: "1",
+    title: "Read a book",
+    description: "Vite + React + TS",
+    isDone: false,
+  },
+  {
+    id: "2",
+    title: "Write code",
+    description: "Finish project",
+    isDone: false,
+  },
+  {
+    id: "3",
+    title: "Deploy app",
+    description: "Push to Vercel",
+    isDone: false,
+  },
+];
+
+const STORAGE_KEY = "lecture13.tasks";
+
+function loadTasks(): TaskCardProps[] {
+  try {
+    const raw = localStorage.getItem(STORAGE_KEY);
+    return raw ? JSON.parse(raw) : defaultTasks;
+  } catch {
+    return defaultTasks; // เผื่อข้อมูลใน localStorage เสีย
+  }
+}
 
 function App() {
-  const [tasks, setTasks] = useState<TaskCardProps[]>([]);
+  const [tasks, setTasks] = useState<TaskCardProps[]>(loadTasks());
 
   const handleAdd = (newTask: TaskCardProps) => {
-    console.log("TODO handleAdd", newTask);
+    setTasks([...tasks, newTask]);
   };
 
   const deleteTask = (taskId: string) => {
-    console.log("TODO deleteTask", taskId);
+    setTasks(tasks.filter((t) => t.id !== taskId));
   };
 
   const toggleDoneTask = (taskId: string) => {
-    console.log("TODO toggleDoneTask", taskId);
+    setTasks(
+      tasks.map((t) => (t.id === taskId ? { ...t, isDone: !t.isDone } : t))
+    );
   };
+
+   useEffect(() => {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+    }, [tasks]);
+
+    const doneCount = tasks.filter((t) => t.isDone).length;
+    const totalCount = tasks.length;
+    const percent = Math.round((doneCount / totalCount) * 100);
+
 
   return (
     <div className="col-12 m-2 p-0">
       <div className="container text-center">
         <h2>Todo List</h2>
-        <span className="m-2">All : () Done : ()</span>
+        <span className="m-2">All : ({tasks.length}) Done : ({tasks.filter((t) => t.isDone).length})</span>
 
+<div
+  className="progress bg-secondary"
+  role="progressbar"
+  aria-valuenow={percent}
+  aria-valuemin={0}
+  aria-valuemax={100}
+>
+  <div className="progress-bar bg-info" style={{ width: `${percent}%` }}>
+    {percent}%
+  </div>
+</div>
         <div>
           <button
             type="button"
